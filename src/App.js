bea705fc534f8e79da50614d9c699b237eef6c46
anuprajvarma <anupraj1854@gmail.com>
2022-08-10 02:45:07 +0530

    Add : add some factionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,46 @@ function App() {
   const [lineColor, setLineColor] = useState("black");
   const [lineOpacity, setLineOpacity] = useState(0.1);
 
+  // Initialization when the component
+  // mounts for the first time
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.globalAlpha = lineOpacity;
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctxRef.current = ctx;
+  }, [lineColor, lineOpacity, lineWidth]);
+
+  // Function for starting the drawing
+  const startDrawing = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY
+    );
+    setIsDrawing(true);
+  };
+
+  // Function for ending the drawing
+  const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = (e) => {
+    if (!isDrawing) {
+      return;
+    }
+    ctxRef.current.lineTo(
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY
+    );
+
+    ctxRef.current.stroke();
+  };
 
   return (
     <div className="App">
@@ -20,7 +60,14 @@ function App() {
           setLineWidth={setLineWidth}
           setLineOpacity={setLineOpacity}
         />
-
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          width={`1280px`}
+          height={`720px`}
+        />
       </div>
     </div>
   );
