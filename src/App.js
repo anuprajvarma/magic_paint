f3c1411e304cc8696c19eb2f46d86b392582e37b
anuprajvarma <anupraj1854@gmail.com>
2022-08-10 02:42:27 +0530

    Create: create menu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import Menu from "./Menu";
 import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(5);
+  const [lineColor, setLineColor] = useState("black");
+  const [lineOpacity, setLineOpacity] = useState(0.1);
 
 
   return (
     <div className="App">
       <h1>Paint App</h1>
+      <div className="draw-area">
+        <Menu
+          setLineColor={setLineColor}
+          setLineWidth={setLineWidth}
+          setLineOpacity={setLineOpacity}
+        />
+
+      </div>
     </div>
   );
 }
