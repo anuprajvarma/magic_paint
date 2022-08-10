edab2f5ce49a575380db0fdcf9275b74a211af0d
anuprajvarma <anupraj1854@gmail.com>
2022-08-10 15:45:38 +0530

    create login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-import Menu from "./Menu";
-import "./App.css";
+import './App.css'
+import Home from './components/homepage/Home'
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
-  const canvasRef = useRef(null);
-  const ctxRef = useRef(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [lineWidth, setLineWidth] = useState(5);
-  const [lineColor, setLineColor] = useState("black");
-  const [lineOpacity, setLineOpacity] = useState(0.1);
 
-  // Initialization when the component
-  // mounts for the first time
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.globalAlpha = lineOpacity;
-    ctx.strokeStyle = lineColor;
-    ctx.lineWidth = lineWidth;
-    ctxRef.current = ctx;
-  }, [lineColor, lineOpacity, lineWidth]);
-
-  // Function for starting the drawing
-  const startDrawing = (e) => {
-    ctxRef.current.beginPath();
-    ctxRef.current.moveTo(
-      e.nativeEvent.offsetX,
-      e.nativeEvent.offsetY
-    );
-    setIsDrawing(true);
-  };
-
-  // Function for ending the drawing
-  const endDrawing = () => {
-    ctxRef.current.closePath();
-    setIsDrawing(false);
-  };
-
-  const draw = (e) => {
-    if (!isDrawing) {
-      return;
-    }
-    ctxRef.current.lineTo(
-      e.nativeEvent.offsetX,
-      e.nativeEvent.offsetY
-    );
-
-    ctxRef.current.stroke();
-  };
-
+  const [user, setLoginUser] = useState({})
   return (
     <div className="App">
-      <h1>Paint App</h1>
-      <div className="draw-area">
-        <Menu
-          setLineColor={setLineColor}
-          setLineWidth={setLineWidth}
-          setLineOpacity={setLineOpacity}
-        />
-        <canvas
-          onMouseDown={startDrawing}
-          onMouseUp={endDrawing}
-          onMouseMove={draw}
-          ref={canvasRef}
-          width={`1280px`}
-          height={`720px`}
-        />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={
+            user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />
+          } />
+          <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
